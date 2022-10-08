186eecf5d201d1c06e23e4dd06de6059e6b0571b
nor3th <73965465+nor3th@users.noreply.github.com>
2022-10-08 23:51:30 +0200

    [mitre] Refactor connector and implement tests

--- external-import/mitre/src/mitre.py
+++ external-import/mitre/src/mitre.py
@@ -1,88 +1,30 @@
 """Mitre connector module."""
 import json
-import os
-import ssl
 import sys
 import time
-import urllib
-from datetime import datetime
-from typing import Optional
+from typing import List
 
-import certifi
-import yaml
-from pycti import OpenCTIConnectorHelper, get_config_variable
+from stix2 import Bundle, parse
+
+from pycti.connector.new.connector_types.connector_base_types import (
+    ExternalInputConnector,
+)
+from pycti.connector.new.connector_types.connector_settings import ConnectorConfig
+from pycti.connector.new.libs.mixins.http import HttpMixin
 
 
-class Mitre:
+class MitreConfig(ConnectorConfig):
+    enterprise_file_url: str = "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/enterprise-attack/enterprise-attack.json"
+    mobile_attack_file_url: str = "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/mobile-attack/mobile-attack.json"
+    ics_attack_file_url: str = "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master/ics-attack/ics-attack.json"
+    capec_file_url: str = (
+        "https://raw.githubusercontent.com/mitre/cti/master/capec/2.1/stix-capec.json"
+    )
+
+
+class MitreConnector(ExternalInputConnector, HttpMixin):
     """Mitre connector."""
-
-    def __init__(self):
-        # Instantiate the connector helper from config
-        config_file_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yml"
-        config = (
-            yaml.load(open(config_file_path), Loader=yaml.FullLoader)
-            if os.path.isfile(config_file_path)
-            else {}
-        )
-        self.helper = OpenCTIConnectorHelper(config)
-        # Extra config
-        self.mitre_enterprise_file_url = get_config_variable(
-            "MITRE_ENTERPRISE_FILE_URL", ["mitre", "enterprise_file_url"], config
-        )
-        self.mitre_pre_attack_file_url = get_config_variable(
-            "MITRE_PRE_ATTACK_FILE_URL", ["mitre", "pre_attack_file_url"], config
-        )
-        self.mitre_mobile_attack_file_url = get_config_variable(
-            "MITRE_MOBILE_ATTACK_FILE_URL", ["mitre", "mobile_attack_file_url"], config
-        )
-        self.mitre_ics_attack_file_url = get_config_variable(
-            "MITRE_ICS_ATTACK_FILE_URL", ["mitre", "ics_attack_file_url"], config
-        )
-        self.mitre_capec_file_url = get_config_variable(
-            "MITRE_CAPEC_FILE_URL", ["mitre", "capeck_file_url"], config
-        )
-        self.mitre_interval = get_config_variable(
-            "MITRE_INTERVAL", ["mitre", "interval"], config, True
-        )
-        self.update_existing_data = get_config_variable(
-            "CONNECTOR_UPDATE_EXISTING_DATA",
-            ["connector", "update_existing_data"],
-            config,
-        )
-
-    def get_interval(self):
-        return int(self.mitre_interval) * 60 * 60 * 24
-
-    def retrieve_data(self, url: str) -> Optional[str]:
-        """
-        Retrieve data from the given url.
-
-        Parameters
-        ----------
-        url : str
-            Url to retrieve.
-
-        Returns
-        -------
-        str
-            A string with the content or None in case of failure.
-        """
-        try:
-            return (
-                urllib.request.urlopen(
-                    url,
-                    context=ssl.create_default_context(cafile=certifi.where()),
-                )
-                .read()
-                .decode("utf-8")
-            )
-        except (
-            urllib.error.URLError,
-            urllib.error.HTTPError,
-            urllib.error.ContentTooShortError,
-        ) as urllib_error:
-            self.helper.log_error(f"Error retrieving url {url}: {urllib_error}")
-        return None
+    config = MitreConfig
 
     # Add confidence to every object in a bundle
     def add_confidence_to_bundle_objects(self, serialized_bundle: str) -> str:
@@ -105,132 +47,32 @@ class Mitre:
             object_type = obj["type"]
             if object_type in object_types_with_confidence:
                 # self.helper.log_info(f"Adding confidence to {object_type} object")
-                obj["confidence"] = int(self.helper.connect_confidence_level)
+                obj["confidence"] = int(self.base_config.confidence_level)
         return json.dumps(stix_bundle)
 
-    def process_data(self):
-        try:
-            # Get the current timestamp and check
-            timestamp = int(time.time())
-            current_state = self.helper.get_state()
-            if current_state is not None and "last_run" in current_state:
-                last_run = current_state["last_run"]
-                self.helper.log_info(
-                    "Connector last run: "
-                    + datetime.utcfromtimestamp(last_run).strftime("%Y-%m-%d %H:%M:%S")
-                )
-            else:
-                last_run = None
-                self.helper.log_info("Connector has never run")
-            # If the last_run is more than interval-1 day
-            if last_run is None or (
-                (timestamp - last_run) > ((int(self.mitre_interval) - 1) * 60 * 60 * 24)
-            ):
-                self.helper.log_info("Connector will run!")
+    def run(self, config: MitreConfig) -> (str, List[Bundle]):
+        bundles = []
+        entities = [
+            config.enterprise_file_url,
+            config.mobile_attack_file_url,
+            config.ics_attack_file_url,
+            config.capec_file_url,
+        ]
 
-                now = datetime.utcfromtimestamp(timestamp)
-                friendly_name = "MITRE run @ " + now.strftime("%Y-%m-%d %H:%M:%S")
-                work_id = self.helper.api.work.initiate_work(
-                    self.helper.connect_id, friendly_name
-                )
-                # MITRE enterprise file url
-                if (
-                    self.mitre_enterprise_file_url is not None
-                    and len(self.mitre_enterprise_file_url) > 0
-                ):
-                    enterprise_data = self.retrieve_data(self.mitre_enterprise_file_url)
-                    enterprise_data_with_confidence = (
-                        self.add_confidence_to_bundle_objects(enterprise_data)
-                    )
-                    self.send_bundle(work_id, enterprise_data_with_confidence)
+        for entity in entities:
+            self.logger.info(f"Retrieving {entity}")
+            data = self.get(entity)
+            data_with_confidence = self.add_confidence_to_bundle_objects(data)
+            obj = parse(data_with_confidence, allow_custom=True)
+            bundles.append(obj)
 
-                # MITRE mobile attack file url
-                if (
-                    self.mitre_mobile_attack_file_url is not None
-                    and len(self.mitre_mobile_attack_file_url) > 0
-                ):
-                    mobile_attack_data = self.retrieve_data(
-                        self.mitre_mobile_attack_file_url
-                    )
-                    mobile_attack_data_with_confidence = (
-                        self.add_confidence_to_bundle_objects(mobile_attack_data)
-                    )
-                    self.send_bundle(work_id, mobile_attack_data_with_confidence)
-
-                # MITRE ICS attack file url
-                if (
-                    self.mitre_ics_attack_file_url is not None
-                    and len(self.mitre_ics_attack_file_url) > 0
-                ):
-                    ics_attack_data = self.retrieve_data(self.mitre_ics_attack_file_url)
-                    ics_attack_data_with_confidence = (
-                        self.add_confidence_to_bundle_objects(ics_attack_data)
-                    )
-                    self.send_bundle(work_id, ics_attack_data_with_confidence)
-
-                # MITRE CAPEC attack file url
-                if (
-                    self.mitre_capec_file_url is not None
-                    and len(self.mitre_capec_file_url) > 0
-                ):
-                    capec_data = self.retrieve_data(self.mitre_capec_file_url)
-                    capec_data_with_confidence = self.add_confidence_to_bundle_objects(
-                        capec_data
-                    )
-                    self.send_bundle(work_id, capec_data_with_confidence)
-
-                # Store the current timestamp as a last run
-                message = "Connector successfully run, storing last_run as " + str(
-                    timestamp
-                )
-                self.helper.log_info(message)
-                self.helper.set_state({"last_run": timestamp})
-                self.helper.api.work.to_processed(work_id, message)
-                self.helper.log_info(
-                    "Last_run stored, next run in: "
-                    + str(round(self.get_interval() / 60 / 60 / 24, 2))
-                    + " days"
-                )
-            else:
-                new_interval = self.get_interval() - (timestamp - last_run)
-                self.helper.log_info(
-                    "Connector will not run, next run in: "
-                    + str(round(new_interval / 60 / 60 / 24, 2))
-                    + " days"
-                )
-        except (KeyboardInterrupt, SystemExit):
-            self.helper.log_info("Connector stop")
-            sys.exit(0)
-        except Exception as e:
-            self.helper.log_error(str(e))
-
-    def run(self):
-        self.helper.log_info("Fetching MITRE datasets...")
-        get_run_and_terminate = getattr(self.helper, "get_run_and_terminate", None)
-        if callable(get_run_and_terminate) and self.helper.get_run_and_terminate():
-            self.process_data()
-            self.helper.force_ping()
-        else:
-            while True:
-                self.process_data()
-                time.sleep(60)
-
-    def send_bundle(self, work_id: str, serialized_bundle: str) -> None:
-        try:
-            self.helper.send_stix2_bundle(
-                serialized_bundle,
-                entities_types=self.helper.connect_scope,
-                update=self.update_existing_data,
-                work_id=work_id,
-            )
-        except Exception as e:
-            self.helper.log_error(f"Error while sending bundle: {e}")
+        return "Finished", bundles
 
 
 if __name__ == "__main__":
     try:
-        mitreConnector = Mitre()
-        mitreConnector.run()
+        mitreConnector = MitreConnector()
+        mitreConnector.start()
     except Exception as e:
         print(e)
         time.sleep(10)
---
+++ external-import/mitre/tests/connector_setup.py
@@ -0,0 +1,29 @@
+import uuid
+
+from stix2 import Bundle
+from src.mitre import MitreConnector
+from pycti.connector.new.tests.test_class import ConnectorTest
+
+
+class MitreTest(ConnectorTest):
+    connector = MitreConnector
+
+    def _setup(self, monkeypatch):
+        monkeypatch.setenv("opencti_broker", "pika")
+        monkeypatch.setenv("opencti_ssl_verify", "False")
+        monkeypatch.setenv("connector_id", str(uuid.uuid4()))
+        monkeypatch.setenv("connector_name", "MITRE Connector")
+        monkeypatch.setenv("connector_run_and_terminate", "true")
+        monkeypatch.setenv("connector_scope", 'IPv4Addr')
+        monkeypatch.setenv("connector_interval", "2")
+        monkeypatch.setenv("connector_testing", "True")
+        monkeypatch.setenv("app_enterprise_file_url", "https://raw.githubusercontent.com/oasis-open/cti-stix-common-objects/main/objects/location/location--011a9d8e-75eb-475a-a861-6998e9968287.json")
+
+    def verify(self, bundle: Bundle):
+        bundle_objects = bundle["objects"]
+        ids = ["bundle--8376c00f-48c6-4fc0-949f-06e8edd7c0a6", "bundle--1bbffad5-534d-4349-8ab9-6cef05a8676d", "bundle--17f97d69-4677-4371-9fd4-a18ae702576f", "bundle--adac1215-a6be-4f71-84ad-8d467e1dc412", "bundle--2c804425-6cae-4444-a630-89f96da3d899"]
+        assert bundle['id'] in ids, f"Invalid bundle id {bundle['id']}"
+        print(f"Len bundles {len(bundle_objects)}")
+
+
+CONNECTORS = [MitreTest]
